73ac733c9298be68791f57e6136c615ef7ddc943
stiproot <stipcich.simon@gmail.com>
2023-08-22 22:15:51 +0200

    fix:setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 # metadata
-name = "pyxi_couchbase_client"
+name = "pyxi_kafka_client"
 version = "0.0.1"
-description = "A basic couchbase client for python."
+description = "A basic kafka client for python."
 long_description = ""
 author = "Simon Stipcich"
 author_email = "stipcich.simon@gmail.com"
-url = "https://github.com/stiproot/py-couchbase-client"
+url = "https://github.com/stiproot/py-kafka-client"
 license = "MIT"
-keywords = ["python", "package", "couchbase", "beta"]
+keywords = ["python", "package", "kafka", "beta"]
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 # dependencies
 install_requires = [
-    "dotenv",
-    "couchbase",
+    "environs",
+    "confluent_kafka",
 ]
 
 # setup
