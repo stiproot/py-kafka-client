7863e7beb15ee13d7537525c7f019c1ac88d0621
stiproot <stipcich.simon@gmail.com>
2023-09-08 19:03:20 +0200

    feat:moving things around.

--- src/pyxi_kafka_client/secret_provider.py
+++ src/pyxi_kafka_client/secret_provider.py
@@ -1,11 +1,10 @@
-import os
 from environs import Env
 
 
 class SecretProvider:
     def __init__(self):
         self._env = Env()
-        self._env.read_env()
+        self._env.read_env(".env")
 
     def get_secret(self, secret_name: str) -> str:
         return self._env(secret_name)
